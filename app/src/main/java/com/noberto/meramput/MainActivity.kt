4fe0c7e87b6b37af571c3b7e07726bcf3aa6330e
Noberto <noberto.1411@gmail.com>
2020-12-13 17:50:03 +0700

    Initial commit

---
+++ app/src/main/java/com/noberto/meramput/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.noberto.meramput
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.noberto.meramput.R.layout.belajar_constrain_layout
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(belajar_constrain_layout)
+    }
+}
